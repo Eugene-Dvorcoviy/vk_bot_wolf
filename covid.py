c3958a433ac7719eac21a7feccd2a9e18af61ea2
Eugene-Dvorcoviy <evgeniy.zharov.2002@mail.ru>
2020-04-22 15:07:22 +0300

    wolf_bot

---
+++ covid.py
@@ -0,0 +1,60 @@
+import COVID19Py
+import requests
+from datetime import datetime
+
+
+class Covid19:
+    def __init__(self):
+        self.covid19 = COVID19Py.COVID19()
+
+    def check_in_the_world(self):
+        now = datetime.now()
+        confirmed = self.covid19.getLatest()["confirmed"]
+        deaths = self.covid19.getLatest()["deaths"]
+
+        return "Данные по всему миру на {}.{}.{}\nЗараженные: {}\nУмерли: {}".format(
+            now.day, now.month, now.year, confirmed,  deaths)
+
+    def check_in_the_russia(self):
+        location = self.covid19.getLocationByCountryCode("RU")
+        deaths = location[0]["latest"]["deaths"]
+        confirmed = int(location[0]["latest"]["confirmed"])
+        update = location[0]["last_updated"]
+        date = update.split('T')[0]
+        time = update.split('T')[1].split('.')[0]
+        population = int(location[0]["country_population"])
+
+        return "Данные по стране Россия:\nПоследнее обновление: {}, {}\nЗаражённых: {}" \
+               "\nУмерло: {}\n%зараженных: {}%".format(date.replace("-", "."), time, confirmed, deaths,
+                                                       confirmed / population * 100)
+
+    def check_in_the_usa(self):
+        location = self.covid19.getLocationByCountryCode("US")
+        deaths = location[0]["latest"]["deaths"]
+        confirmed = int(location[0]["latest"]["confirmed"])
+        update = location[0]["last_updated"]
+        date = update.split('T')[0]
+        time = update.split('T')[1].split('.')[0]
+        population = int(location[0]["country_population"])
+
+        return "Данные по стране США:\nПоследнее обновление: {}, {}\nЗаражённых: {}" \
+               "\nУмерло: {}\n%зараженных: {}%".format(date.replace("-", "."), time, confirmed, deaths,
+                                                       confirmed / population * 100)
+
+    def covid_in_different_country(self, kod):
+        country_cod = kod[1:].upper()
+
+        location = self.covid19.getLocationByCountryCode(country_cod)
+        deaths = location[0]["latest"]["deaths"]
+        confirmed = int(location[0]["latest"]["confirmed"])
+        update = location[0]["last_updated"]
+        date = update.split('T')[0]
+        time = update.split('T')[1].split('.')[0]
+        population = int(location[0]["country_population"])
+        country = location[0]["country"]
+
+        return "Данные по стране {}:\nПоследнее обновление: {}, {}\nЗаражённых: {}" \
+               "\nУмерло: {}\n%зараженных: {}%".format(country, date.replace("-", "."), time, confirmed, deaths,
+                                                       confirmed / population * 100)
+
+
